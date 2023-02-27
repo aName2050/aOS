0a289cd06758a1f0c0b29809d07dcd0f669dae80
Bardia Shafaee <shafaeebardia@gmail.com>
2023-02-26 19:10:51 -0800

    added kernel and kernel entry program

--- src/boot.asm
+++ src/boot.asm
@@ -1,5 +1,29 @@
-[org 0x7c00] 
-mov [BOOT_DISK], dl 
+[org 0x7c00]
+KERNEL_LOCATION equ 0x1000
+
+BOOT_DISK: db 0
+mov [BOOT_DISK], dl
+
+xor ax, ax
+mov es, ax
+mov ds, ax
+mov bp, 0x8000
+mov sp, bp
+
+mov bx, KERNEL_LOCATION
+mov dh, 20
+
+mov ah, 0x02
+mov al, dh
+mov ch, 0x00
+mov dh, 0x00
+mov cl, 0x02
+mov dl, [BOOT_DISK]
+int 0x13
+
+mov ah, 0x0
+mov al, 0x3
+int 0x10
 
 CODE_SEG equ code_descriptor - GDT_Start
 DATA_SEG equ data_descriptor - GDT_Start
@@ -8,11 +32,8 @@ cli
 lgdt [GDT_Descriptor]
 mov eax, cr0
 or eax, 1
-mov cr0, eax ; CPU now in 32-bit protected mode
+mov cr0, eax
 jmp CODE_SEG:start_protected_mode
-
-jmp $
-
 
 GDT_Start:
     null_descriptor:
@@ -40,17 +61,19 @@ GDT_Descriptor:
     dw GDT_End - GDT_Start - 1 ; size
     dd GDT_Start ; start
 
-
 [bits 32]
 start_protected_mode:
-    mov al, 'A'
-    mov ah, 0x0f
-    mov [0xb8000], ax
-    jmp $
+    mov ax, DATA_SEG ; setup segment registers and stack
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ebp, 0x90000
+    mov esp, ebp
 
-BOOT_DISK: db 0
+    jmp KERNEL_LOCATION ; jump to kernel
 
 ; boot sector padding
-
 times 510-($-$$) db 0
 dw 0xaa55
